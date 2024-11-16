050bf7d00396e48a50c9c9fffb180328b4668a6f
Caiopavan <168437221+Caiopavan@users.noreply.github.com>
2024-11-16 00:43:22 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,106 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O Python é uma linguagem de marcação'
+)
+
+criaCartao(
+    'Games',
+    'Quando fortnite foi lançado?',
+    '2011'
+)
+
+
+criaCartao(
+    `Games`,
+    `Jogo mais vendido`,
+    `Minecraft`
+)
+
+
+criaCartao(
+    `Games`,
+    `Jogo mais vendido`,
+    `Minecraft`
+)
+
+
+criaCartao(
+    `Games`,
+    `Jogo mais vendido`,
+    `Minecraft`
+)
+
+
+
+criaCartao(
+    `Games`,
+    `Jogo mais vendido`,
+    `Minecraft`
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
